dee20df6db4f7a9010441b4b7aeb18e246e347a7
HowardLei <32949619+HowardLei@users.noreply.github.com>
2018-01-25 14:01:08 +0800

    Update ViewController.swift

--- day2/按钮的状态/按钮的状态/ViewController.swift
+++ day2/按钮的状态/按钮的状态/ViewController.swift
@@ -6,6 +6,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         demo1()
         demo2()
+        demo3()
     }
    func demo1(x: String) -> (String){
        return x
@@ -13,5 +14,10 @@ class ViewController: UIViewController {
     func demo2(){
        let a = demo1
        print(a("怎么样，我现在就调用你"))
+    }
+    func demo3(){
+       var x = 0
+        x = 10
+        print(x)
     }
 }
