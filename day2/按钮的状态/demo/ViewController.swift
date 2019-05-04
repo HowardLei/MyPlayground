ac6250902daea9459909e204e0402c1dc8c98f5d
HowardLei <32949619+HowardLei@users.noreply.github.com>
2019-05-04 18:08:21 +0800

    修改代码

--- day2/按钮的状态/demo/ViewController.swift
+++ day2/按钮的状态/demo/ViewController.swift
@@ -32,13 +32,57 @@ class ViewController: UIViewController, UITableViewDataSource {
     @IBAction func sliderChange(_ sender: UISlider) {
          myView.progress = CGFloat(sender.value)
     }
+    func rotate(_ nums: inout [Int], _ k: Int) {
+        var dict = [Int: Int](minimumCapacity: nums.count)
+        for i in 0..<nums.count {
+            dict[(i + k) % nums.count] = nums[i]
+        }
+        for i in 0..<nums.count {
+            nums[i] = dict[i]!
+        }
+    }
+    func containsDuplicate(_ nums: [Int]) -> Bool {
+        return nums.count != Set<Int>(nums).count
+//        if nums.count <= 1 {
+//            return false
+//        }
+//        var values = Set<Int>(arrayLiteral: nums[0])
+//        for i in 1..<nums.count {
+//            guard !values.contains(nums[i]) else {
+//                return true
+//            }
+//            values.insert(nums[i])
+//        }
+//        return false
+    }
     func intersect(_ nums1: [Int], _ nums2: [Int]) -> [Int] {
-        var values = [Int]()
+        var dict1 = [Int: Int]()
+        var dict2 = [Int: Int]()
+        var count = 0
+        var res = [Int]()
         for elem in nums1 {
-            if nums2.contains(elem) && values.count < min(nums1.count, nums2.count){
-                values.append(elem)
+            if dict1.keys.contains(elem) {
+                count = dict1[elem]! + 1
+                dict1[elem] = count
+            } else {
+                dict1[elem] = 1
             }
         }
-        return values
+        for elem in nums2 {
+            if dict2.keys.contains(elem) {
+                count = dict2[elem]! + 1
+                dict2[elem] = count
+            } else {
+                dict2[elem] = 1
+            }
+        }
+        dict1.forEach { (k, v) in
+            if dict2.keys.contains(k) {
+                for _ in 0..<min(v, dict2[k]!) {
+                    res.append(k)
+                }
+            }
+        }
+        return res
     }
 }
